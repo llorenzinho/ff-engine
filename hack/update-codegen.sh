c4a9659836adf3bec386f210430fbc9cccfcd376
LorenzoDag <lorenzo-dagostino@libero.it>
2023-11-05 17:25:49 +0100

    updated update-codegen file

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,7 +19,7 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; [ -d ~/go/pkg/mod/k8s.io/code-generator@v0.28.3 ] && echo ~/go/pkg/mod/k8s.io/code-generator@v0.28.3 || echo /go/pkg/mod/k8s.io/code-generator@v0.28.3 )}
 
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
